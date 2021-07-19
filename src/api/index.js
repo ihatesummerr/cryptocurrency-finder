69376909a588be667b7e7117b9c92fd2baec8464
ihatesummerr <ihatesummerr@yahoo.com>
2021-07-19 23:33:26 +0500

    api url changed

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({
+    baseURL: 'https://cryptocurrency-finder.herokuapp.com',
+});
 
 export const fetchCurrency = async () => API.get('/currency');
